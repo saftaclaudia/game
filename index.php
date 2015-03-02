f94ea8ed25bc80b5098837de54ef2c1f4f33952f
saftaclaudia <saftaclaudia27@yahoo.com>
2015-03-02 23:24:48 +0200

    delete knockout
    delete knockout file from assets folder and knockout link from php file

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
 		<div class='square'></div>
 	</div>
 
-	<script type='text/javascript' src='assets/knockout-3.2.0.js'></script>
 	<script type='text/javascript' src='assets/jquery-1.11.1.min.js'></script>
 	<script type='text/javascript' src='assets/bootstrap.min.js'></script>
 	<script type='text/javascript' src="function.js"></script>
