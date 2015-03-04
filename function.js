516489621a613123f65a065b0afad62bb94d662f
saftaclaudia <saftaclaudia27@yahoo.com>
2015-03-05 00:09:41 +0200

    plugin
    initialposition plugin

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+ /*$(document).ready(function() {
 	var gamespaceHeight = $('.game-space').height(),
 			gamespaceWidth = $('.game-space').width(),
 			squareHeight = $('.square').height(),
@@ -16,6 +16,42 @@ $(document).ready(function() {
 	$(".square").click(function(){
 		intialPosition();
 	});
-});
+}); 
+*/
+
+ (function ( $ ) {
+	$.fn.initialPosition = function(options) {
+		var gamespaceHeight = $('.game-space').height();
+		var gamespaceWidth=  $('.game-space').width();
+		var squareHeight=  $('.square').height();
+		var squareWidth=  $('.square').width();
+		var maxHeight= gamespaceHeight - squareHeight;
+		var maxWidth= gamespaceWidth - squareWidth;
+		var settings = $.extend ( {
+			left: Math.floor( Math.random() * maxWidth),
+			top: Math.floor( Math.random()* maxHeight)
+		}, options)
+
+		this.css({
+			left:settings.left,
+			top:settings.top
+		});
+
+		this.click(function(){
+			var settings = $.extend ( {
+				left: Math.floor( Math.random() * maxWidth),
+				top: Math.floor( Math.random()* maxHeight)
+			}, options);
+		
+			$(this).css({
+				left:settings.left,
+				top:settings.top
+			});
+		});
+	};
+}(jQuery));
+	$(".square").initialPosition ();
+
+
 
 
