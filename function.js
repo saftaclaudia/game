e680108f2dba8f000b8d0798d5ee7137317a3687
saftaclaudia <saftaclaudia27@yahoo.com>
2015-05-20 20:29:57 +0300

    refactor

--- function.js
+++ function.js
@@ -1,14 +1,34 @@
 (function ( $ ) {
 	$.fn.gameSettings = function(options) {
-		var maxsec = 4;
-		var $square = $('<div class = "square" ></div>');
-		var $display = $('<div class= "display-zone center-block"></div>');
-		var $score = $('<div class="display"> your score <span>0</span></div>');
-		var $time = $('<div class="timekeep"> <span>'+maxsec+'</span></div>');
-		var $start = $('<button type="button" class="btn btn-primary btn-lg active start-button">Start</button>');
-		var highestScore = getHighestScore();
-		var $highestScore = $('<div class="display"> your highest score <span>'+highestScore+'</span></div>');
-		var $maxscoremessage = $('<div class="success message">awesome game </div>');
+		var maxsec = 4,
+			clicksOut =0,
+			clicks= 0,
+			scoreDecrement = 0,
+			score = 1,
+			sec = maxsec,
+			$square = $('<div class = "square" ></div>'),
+			$display = $('<div class= "display-zone center-block"></div>'),
+			$score = $('<div class="display"> your score <span>0</span></div>'),
+			$time = $('<div class="timekeep"> <span>'+maxsec+'</span></div>'),
+			$start = $('<button type="button" class="btn btn-primary btn-lg active start-button">Start</button>'),
+			highestScore = getHighestScore(),
+			$highestScore = $('<div class="display"> your highest score <span>'+highestScore+'</span></div>'),
+			$maxscoremessage = $('<div class="success message">awesome game </div>'),
+			defaultSettings = {
+				gamespaceHeight: this.height(),
+				gamespaceWidth: this.width(),
+				squareHeight: $square.height(),
+				squareWidth: $square.width(),
+				decrementSquare:2,
+			},
+			settings = $.extend( defaultSettings, options ),
+
+			initialSquareWidth = settings.squareWidth,
+			initialSquareHeight = settings.squareHeight,
+			squareWidth = initialSquareWidth,
+			squareHeight = initialSquareHeight,
+			maxHeight= settings.gamespaceHeight - settings.squareHeight,
+			maxWidth= settings.gamespaceWidth - settings.squareWidth;
 
 		this.append($square);
 		this.append($maxscoremessage);
@@ -16,17 +36,6 @@
 		$display.append($start).append($time).append($score).append($highestScore);
 		$square.hide();
 		$maxscoremessage.hide();
-		
-		var settings = $.extend({
-			gamespaceHeight: this.height(),
-			gamespaceWidth: this.width(),
-			squareHeight: $square.height(),
-			squareWidth: $square.width(),
-			decrementSquare:2,
-		}, options);
-
-		var maxHeight= settings.gamespaceHeight - settings.squareHeight;
-		var maxWidth= settings.gamespaceWidth - settings.squareWidth;
 
 		$square.css({
 			height: settings.squareHeight,
@@ -36,8 +45,6 @@
 			height: settings.gamespaceHeight,
 			width: settings.gamespaceWidth
 		});
-
-		var sec = maxsec;
 
 		var decrement = function() {
 			setTimeout(function(){
@@ -52,36 +59,29 @@
 		};
 
 		$start.on('click', startGame);
-		$square.on('click', gameActions);
-		$square.on('click', decrementSize);
-		$square.on('click', setScore);
-
+		
 		$square.click(function(event){
 			event.stopPropagation();
-			getClicks();
-			console.log(clicks);
+			gameActions();
+			decrementSize();
+			setScore();
+			clicks++;
 		});
 
-		this.on('click', setScoreDecrement);
-		this.on('click',decrementScore);
-		this.on('click', getClicksOut);
-		
+		this.click(function(){
+			clicksOut++;
+			if (clicksOut >3){
+				clicksOut =0;
+			};
+			setScoreDecrement();
+			decrementScore();
+		});
+
 		function randomPosition() {
 			$square.animate({
 				left: Math.floor( Math.random() * maxWidth),
 				top: Math.floor( Math.random()* maxHeight)
 			}, 'slow');
-		};
-
-		var clicksOut =0;
-		function getClicksOut(){
-			clicksOut++;
-			console.log(clicksOut);
-		};
-
-		var clicks= 0;
-		function getClicks(){
-			clicks ++;
 		};
 
 		function getScore(){
@@ -95,47 +95,44 @@
 			};
 		};
 
-		var scoreDecrement = 0;
-
 		function setScoreDecrement(){
 
 			if(squareWidth <= 40 && squareWidth>30){
-				scoreDecrement= 30;
+				scoreDecrement= 4;
 			};
 			if(squareWidth <=30 && squareWidth>20){
-				scoreDecrement= 20;
+				scoreDecrement= 3;
 			};
 			if(squareWidth<=20){
-				scoreDecrement= 10;
+				scoreDecrement= 2;
 			};
 		};
 
-		var score = 1;
 		function incrementScore() {
 			var totalScore= getScore() +score;
 			$score.find('span').text(totalScore);
 		};
 		
 		function decrementScore(){
-			if (sec >0) {
-				var totalScore= getScore() -scoreDecrement;
+			if (sec > 0) {
+				var totalScore = getScore() - scoreDecrement;
 				$score.find('span').text(totalScore);
 			};
-			if(sec> 0 && clicksOut>=0){
-				var totalScore= getScore() -1;
+			if(sec > 0 && clicksOut > 0){
+				var totalScore = getScore() - 1;
 				$score.find('span').text(totalScore);
+			};
+			if(clicksOut === 3){
+				var totalScore = 0;
+				$score.find('span').text(totalScore);
+				console.log(totalScore);
 			};
 		};
 
-		var initialSquareWidth = $square.width();
-		var initialSquareHeight = $square.height();
-		var squareWidth = initialSquareWidth;
-		var squareHeight = initialSquareHeight;
-
 		function decrementSize(){
-			if(clicks%2 !=0 && clicks>2 && squareWidth>2){
-				squareWidth-=settings.decrementSquare;
-				squareHeight-=settings.decrementSquare;
+			if(clicks % 2 != 0 && clicks > 2 && squareWidth > 2){
+				squareWidth -= settings.decrementSquare;
+				squareHeight -= settings.decrementSquare;
 			};
 				$square.css({
 				width: squareWidth,
@@ -154,9 +151,9 @@
 		function startGame() {
 			$square.show();
 			clicks = 0;
-			clicksOut=0;
-			score= 1;
-			scoreDecrement=0;
+			clicksOut =0;
+			score = 1;
+			scoreDecrement = 0;
 			squareWidth = initialSquareWidth;
 			squareHeight = initialSquareHeight;
 
@@ -183,7 +180,7 @@
 			if (totalScore > highestScore) {
 				localStorage.setItem('highestScore', totalScore);
 				$highestScore.find('span').text(totalScore);
-				$maxscoremessage.show();
+				$maxscoremessage.fadeIn("slow");
 			}
 		};
 
@@ -194,7 +191,6 @@
 			}
 			return result;
 		};
-
 	};
 
 }(jQuery));
